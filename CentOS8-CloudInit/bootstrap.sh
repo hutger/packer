caf8b98d06e341df767bc4d11542fab66369703d
Hutger H <hutger@gmail.com>
2020-11-03 18:47:38 +0100

    Adding ubuntu seeds

--- CentOS8-CloudInit/bootstrap.sh
+++ CentOS8-CloudInit/bootstrap.sh
@@ -1,9 +1,8 @@
 #/bin/sh
 set -ex
 
-# Update packages
-#sudo dnf update -y
-
 # Install Openview Agent - https://www.opsview.com/product/downloads
 sudo dnf install -y epel-release
 sudo dnf install -y ansible cloud-init
+sudo plymouth-set-default-theme details
+sudo dracut -f
